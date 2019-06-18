d51c47650baca22c45600ff5787de924790ddff6
Dharani-18 <50835212+Dharani-18@users.noreply.github.com>
2019-06-18 15:46:22 +0530

    Create palindrome.c

---
+++ pgm/palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n,remain,reverse=0;
+    scanf("%d",&n); 
+      while(n != 0)
+    {
+        remain = n%10;
+        reverse = reverse*10 + remain;
+        n /= 10;
+    }
+    printf("%d",reverse);
+    if(n==reverse)
+    {
+    printf("no");
+    }
+    else
+    printf("yes");
+     return 0;
+}
