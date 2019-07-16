95ed01c5eacfac42038b0b875980c854bfb79ea0
Dharani-18 <50835212+Dharani-18@users.noreply.github.com>
2019-07-16 10:58:50 +0530

    Update armstrong no.py

--- pgm/armstrong no.py
+++ pgm/armstrong no.py
@@ -1,12 +1,12 @@
-num = int(input())
-sum = 0
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
-
-if num == sum:
-   print("Yes")
-else:
-   print("No")
+num1 =int(input())
+num2=int(input())
+for num in range(num1,num2):
+    order = len(str(num1))
+    sum = 0
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if num == sum:
+        print(num)
