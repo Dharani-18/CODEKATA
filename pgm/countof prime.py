ce0befe5a370613908783e838d9334ef5392ed5e
Dharani-18 <50835212+Dharani-18@users.noreply.github.com>
2019-07-25 20:57:28 +0530

    Create countof prime.py

---
+++ pgm/countof prime.py
@@ -0,0 +1,12 @@
+
+lower,upper=map(int,input().split())
+count = 0
+for num in range (lower, upper+1):
+    
+ if num>1:
+  for i in range (2,num):
+   if num % i==0:
+    break
+  else: 
+    count += 1
+print (count) 
